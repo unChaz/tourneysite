412a3f15989cb13db0904e86db3701c99583bb75
Chaz Ferguson <chaz.ferguson@gmail.com>
2017-12-31 16:49:10 -0500

    fix time

--- app/models/server_config.rb
+++ app/models/server_config.rb
@@ -2,7 +2,7 @@ class ServerConfig
   def self.load
     {
       site_title: 'Ting Open',
-      live_date: (DateTime.now + 5.seconds).to_s,#DateTime.new(2018,1,10,1).to_s,
+      live_date: DateTime.new(2018,1,10,1).to_s,
       twitter_widget_id: '947533030889082880',
       twitter_hashtag: 'TingOpen',
       twitch_channel: 'axeltoss',
