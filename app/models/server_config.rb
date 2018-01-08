28eb9ad26505ae77ef6a36cfa6c03b5432335936
Chaz Ferguson <chaz.ferguson@gmail.com>
2018-01-08 10:41:10 -0500

    push back start time

--- app/models/server_config.rb
+++ app/models/server_config.rb
@@ -2,11 +2,11 @@ class ServerConfig
   def self.load
     {
       site_title: 'Ting Open',
-      live_date: DateTime.new(2018,1,10,1).to_s,
+      live_date: DateTime.new(2018,1,10,2).to_s,
       twitter_widget_id: '947533030889082880',
       twitter_hashtag: 'TingOpen',
       twitch_channel: 'axeltoss',
-      start_time_string: 'January 9, 2018 5PM PST'
+      start_time_string: 'January 9, 2018 6PM PST'
     }
   end
 end
