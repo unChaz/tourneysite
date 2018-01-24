1d96705a01f5d26ad87920d3b72f14ef6e4893d2
Chaz Ferguson <chaz.ferguson@gmail.com>
2018-01-23 19:03:04 -0500

    setup for qualifier 4

--- app/models/server_config.rb
+++ app/models/server_config.rb
@@ -2,12 +2,12 @@ class ServerConfig
   def self.load
     {
       site_title: 'Ting Open',
-      live_date: DateTime.new(2018,1,23,15,0,0,'-7').to_s,
+      live_date: DateTime.new(2018,1,30,15,0,0,'-7').to_s,
       stream_date: DateTime.new(2018,1,11,13,45,0,'-7').to_s,
       twitter_widget_id: '947533030889082880',
       twitter_hashtag: 'TingOpen',
       twitch_channel: 'axeltoss',
-      start_time_string: 'January 16, 6PM PST'
+      start_time_string: 'January 30, 11AM PST'
     }
   end
 end
