05b4b4b0800d3c143d2ab0a3ad1f0c8471ff8c68
Chaz Ferguson <chaz.ferguson@gmail.com>
2018-02-01 10:33:05 -0500

    update for finals

--- app/models/server_config.rb
+++ app/models/server_config.rb
@@ -2,7 +2,7 @@ class ServerConfig
   def self.load
     {
       site_title: 'Ting Open',
-      live_date: DateTime.new(2018,1,30,15,0,0,'-7').to_s,
+      live_date: DateTime.new(2018,2,3,13,0,0,'-7').to_s,
       stream_date: DateTime.new(2018,1,11,13,45,0,'-7').to_s,
       twitter_widget_id: '947533030889082880',
       twitter_hashtag: 'TingOpen',
